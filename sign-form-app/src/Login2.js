d72a4c7159c479f6a4a19455a20d17dd9a858f1e
Yago <yagovr.rocha@gmail.com>
2024-02-13 20:10:57 -0300

    Add validation

--- sign-form-app/src/Login2.js
+++ sign-form-app/src/Login2.js
@@ -5,25 +5,50 @@ export default function App() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    watch,
   } = useForm()
 
+  function check() {
+    var input = document.querySelector('#confirmPassword');
+    if (input.value !== document.querySelector('#password').value) {
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        input.setCustomValidity('');
+    }
+  }
 
+  const password = watch("password");
+  
   const onSubmit = (data) => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="First Name" {...register("firstName" , {required: true})} />
-      {errors.firstName && <span>asdasd</span>}
-      <input type="text" placeholder="Last Name" {...register("lastName" , {required: true})} />
-
-      <input type="date" placeholder="Birthday" {...register("birthday" , {required: true})} />
+      <label>First Name</label>
+      <input name="firstName" type="text" placeholder="First Name" {...register("firstName" , {required: true, maxLength:20 , pattern: /^[A-Za-z]+$/i})} />
       
-      <input type="email" placeholder="Email" {...register("email" , {required: true})} />
+      <label>Last Name</label>
+      <input name="lastName" type="text" placeholder="Last Name" {...register("lastName" , {required: true, pattern: /^[A-Za-z]+$/i})} />
 
-      <input type="password" placeholder="Password" {...register("password" , {required: true})} />
+      <label>Birthday</label>
+      <input name="birthday" type="date" placeholder="Birthday" {...register("birthday" , {required: true})} />
+      
+      <label>Email</label>
+      <input name="email" type="email" placeholder="Email" {...register("email" , {required: true})} />
 
-      <input type="password" placeholder="Confirm Password" {...register("confirmPassword" , {required: true})} />
+      <label>Gender</label>
+      <select name="gender" {...register("gender")}>
+        <option value="">Select your gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+        <option value="none">I prefer not to say</option>
+      </select>
+
+      <label>Password</label>
+      <input name ="password" type="password" id="password" placeholder="Password"  {...register("password", {required: true})} />
+      
+      <label>Confirm your password</label>
+      <input name ="confirmPassword" type="password" id="confirmPassword" placeholder="Confirm Password" {...register("confirmPassword" , {required: true, validate: value => value === password || "Password must be matching!"})} />
       
       <input type="submit" />
     </form>
